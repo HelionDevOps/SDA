866ebcf8a6b771c015ac1a1fec8a00945d02ec52
HelionDevOps <demnerihelion@gmail.com>
2025-02-13 11:34:08 +0100

    New files added also

--- mirror.sh
+++ mirror.sh
@@ -3,9 +3,8 @@
 OLD_repo=$1
 NEW_repo=$2
 
-git clone $OLD_repo repo_temp
+git clone --mirror $OLD_repo repo_temp
 mv .env Hi.sh mirror.sh repo_temp
-git add .
 
 cd repo_temp
 git remote set-url origin $NEW_repo
